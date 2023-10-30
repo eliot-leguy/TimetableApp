ca7222d93ce068856df37d87fe7135b1ec0bf14f
LEGUY Eliot <eliot.leguy@gmail.com>
2023-10-30 20:32:49 +0100

    Synchronization of both hour and events lists to scroll both at once

--- app/src/main/java/perso/edt1/DailyCalendarActivity.java
+++ app/src/main/java/perso/edt1/DailyCalendarActivity.java
@@ -5,9 +5,13 @@ import static perso.edt1.CalendarUtils.selectedDate;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -25,6 +29,10 @@ public class DailyCalendarActivity extends AppCompatActivity
     private ListView hourListView;
     private ListView eventListView;
 
+    private View touchSource;
+    private View clickSource;
+    private final int offset = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -33,12 +41,90 @@ public class DailyCalendarActivity extends AppCompatActivity
         initWidgets();
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private void initWidgets()
     {
         monthDayText = findViewById(R.id.monthDayText);
         dayOfWeekTV = findViewById(R.id.dayOfWeekTV);
         hourListView = findViewById(R.id.hourListView);
         eventListView = findViewById(R.id.eventListView);
+
+
+        //When we scroll one list, we want both to scroll so we have to link them :
+        hourListView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(touchSource == null)
+                    touchSource = v;
+
+                if(v == touchSource) {
+                    eventListView.dispatchTouchEvent(event);
+                    if(event.getAction() == MotionEvent.ACTION_UP) {
+                        clickSource = v;
+                        touchSource = null;
+                    }
+                }
+
+                return false;
+            }
+        });
+        eventListView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(touchSource == null)
+                    touchSource = v;
+
+                if(v == touchSource) {
+                    hourListView.dispatchTouchEvent(event);
+                    if(event.getAction() == MotionEvent.ACTION_UP) {
+                        clickSource = v;
+                        touchSource = null;
+                    }
+                }
+
+                return false;
+            }
+        });
+
+        hourListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(parent == clickSource) {
+                    // Do something with the ListView was clicked
+                }
+            }
+        });
+        eventListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(parent == clickSource) {
+                    // Do something with the ListView was clicked
+                }
+            }
+        });
+
+        hourListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if(view == clickSource)
+                    eventListView.setSelectionFromTop(firstVisibleItem, view.getChildAt(0).getTop() + offset);
+            }
+
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {}
+        });
+        eventListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if(view == clickSource)
+                    hourListView.setSelectionFromTop(firstVisibleItem, view.getChildAt(0).getTop() + offset);
+            }
+
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {}
+        });
+
+
     }
 
     @Override
