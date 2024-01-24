120a803362dd0739a23f6a348671c11b8d17d93a
LEGUY Eliot <eliot.leguy@gmail.com>
2024-01-24 15:36:55 +0100

    Getting the groups and setting the UI in a asynch task

--- app/src/main/java/perso/edt1/EdtSelectionActivity.java
+++ app/src/main/java/perso/edt1/EdtSelectionActivity.java
@@ -1,9 +1,11 @@
 package perso.edt1;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,18 +22,18 @@ public class EdtSelectionActivity extends AppCompatActivity {
     public static String groupsString;
     private static Map<String,String> selectedGroups;
 
-    ScrollView scrollView;
+    LinearLayout groupLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.edt_selection_activity);
-        scrollView = findViewById(R.id.groupsScrollView);
+        groupLinearLayout = findViewById(R.id.groupLinearLayout);
 
         Toast.makeText(this, "Fetching groups", Toast.LENGTH_SHORT).show();
         String url = "https://edt.univ-nantes.fr/chantrerie-gavy/gindex.html";
-        UrlRequests urlRequests = new UrlRequests(this, "GROUPS");
+        UrlRequests urlRequests = new UrlRequests(this, "GROUPS", null);
         urlRequests.execute(url);
     }
 
@@ -39,87 +41,103 @@ public class EdtSelectionActivity extends AppCompatActivity {
     @Override
     protected void onResume(){
         super.onResume();
-
-        getGroups(groupsString);
-        displayGroups();
-
+        getGroups getGroups = new getGroups();
+        getGroups.execute();
     }
 
-    private class getGroups extends AsyncTask<Void, Void, Void> {
-        @Override
-        protected Void doInBackground(Void... voids) {
-            return null;
-        }
-    }
+    @Override
+    protected void onStop() {
+        super.onStop();
 
-
-    /**
-     * Returns a map containing the groups names and id from the inputString.
-     *
-     * @param inputString Context of the application.
-     */
-    void getGroups(String inputString) {
-        String[] lines = inputString.split("\n");
-        groups = new Hashtable<>();
-
-        for (String line : lines) {
-            if(line.contains("<option value")){
-                boolean endID = false;
-                boolean endName = false;
-                StringBuilder id = new StringBuilder();
-                StringBuilder name = new StringBuilder();
-                for (int i = 0; i < line.length(); i++) {
-                    if (endID) {
-                        id.append(line.charAt(i));
-                    }
-                    if (line.charAt(i) == '"') {
-                        endID = !endID;
-                    }
-                    if (line.charAt(i) == '<') {
-                        endName = false;
-                    }
-                    if (endName) {
-                        name.append(line.charAt(i));
-                    }
-                    if (line.charAt(i) == '>') {
-                        endName = true;
-                    }
-                    if (line.charAt(i) == '/') {
-                        //Cut id at the dot
-                        id = new StringBuilder(id.substring(0, id.length() - 6));
-                        groups.put(name.toString(), id.toString());
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    private void displayGroups() {
-
-        ArrayList<String> groupsNames = new ArrayList<>(groups.keySet());
-        selectedGroups = new Hashtable<>();
+        ArrayList<String> groupsNames = new ArrayList<>(selectedGroups.keySet());
 
         for (String groupName : groupsNames) {
+            String url = "https://edt.univ-nantes.fr/chantrerie-gavy/" + selectedGroups.get(groupName) +".xml";
+            UrlRequests urlRequests = new UrlRequests(this, "EDT", groupName);
+            urlRequests.execute(url);
+        }
+    }
 
-            View view = getLayoutInflater().inflate(R.layout.group_selector, null);
-            TextView groupNameTextView = view.findViewById(R.id.groupNameTextView);
-            CheckBox groupCheckBox = view.findViewById(R.id.groupCheckBox);
+    public void closeActivityAction(View view) {
+        finish();
+    }
 
-            groupNameTextView.setText(groupName);
+    private class getGroups extends AsyncTask<Void, Void, Map<String,String> > {
+        @Override
+        protected Map<String,String>  doInBackground(Void... voids) {
+            Map<String,String> groups = new Hashtable<>();
 
-            if(Event.localEdt.contains(groupName)){
-                groupCheckBox.setChecked(true);
-            }
-            groupCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                if (isChecked) {
-                    selectedGroups.put(groupName, groups.get(groupName));
-                } else {
-                    selectedGroups.remove(groupName);
+            while(groupsString.isEmpty()){
+                try{
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
-            });
+            }
 
-            scrollView.addView(view);
+            String[] lines = groupsString.split("\n");
+
+            for (String line : lines) {
+                if(line.contains("<option value")){
+                    boolean endID = false;
+                    boolean endName = false;
+                    StringBuilder id = new StringBuilder();
+                    StringBuilder name = new StringBuilder();
+                    for (int i = 0; i < line.length(); i++) {
+                        if (endID) {
+                            id.append(line.charAt(i));
+                        }
+                        if (line.charAt(i) == '"') {
+                            endID = !endID;
+                        }
+                        if (line.charAt(i) == '<') {
+                            endName = false;
+                        }
+                        if (endName) {
+                            name.append(line.charAt(i));
+                        }
+                        if (line.charAt(i) == '>') {
+                            endName = true;
+                        }
+                        if (line.charAt(i) == '/') {
+                            //Cut id at the dot
+                            id = new StringBuilder(id.substring(0, id.length() - 6));
+                            groups.put(name.toString(), id.toString());
+                            break;
+                        }
+                    }
+                }
+            }
+            return groups;
+        }
+
+        @Override
+        protected void onPostExecute(Map<String,String> groups) {
+            //Update the display
+            ArrayList<String> groupsNames = new ArrayList<>(groups.keySet());
+            selectedGroups = new Hashtable<>();
+
+            for (String groupName : groupsNames) {
+
+                View view = getLayoutInflater().inflate(R.layout.group_selector, null);
+                TextView groupNameTextView = view.findViewById(R.id.groupNameTextView);
+                CheckBox groupCheckBox = view.findViewById(R.id.groupCheckBox);
+
+                groupNameTextView.setText(groupName);
+
+                if(Event.localEdt.contains(groupName)){
+                    groupCheckBox.setChecked(true);
+                }
+                groupCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+                    if (isChecked) {
+                        selectedGroups.put(groupName, groups.get(groupName));
+                    } else {
+                        selectedGroups.remove(groupName);
+                    }
+                });
+
+                groupLinearLayout.addView(view);
+            }
         }
     }
 }
