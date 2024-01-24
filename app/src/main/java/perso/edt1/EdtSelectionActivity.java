f08ab9c39be109ba5e9c04e6bc221e0466216747
Eliot Leguy <eliot.leguy@gmail.com>
2024-01-24 21:56:34 +0100

    Now saving loading the groups from web, or local if available

--- app/src/main/java/perso/edt1/EdtSelectionActivity.java
+++ app/src/main/java/perso/edt1/EdtSelectionActivity.java
@@ -1,6 +1,5 @@
 package perso.edt1;
 
-import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,13 +7,16 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Map;
@@ -35,9 +37,23 @@ public class EdtSelectionActivity extends AppCompatActivity {
         groupLinearLayout = findViewById(R.id.groupLinearLayout);
 
         Toast.makeText(this, "Fetching groups", Toast.LENGTH_SHORT).show();
-        String url = "https://edt.univ-nantes.fr/chantrerie-gavy/gindex.html";
-        UrlRequests urlRequests = new UrlRequests(this, "GROUPS", null);
-        urlRequests.execute(url);
+
+        //Fetch groups from local storage or from the web
+        ArrayList<File> files = getFiles(new File(getFilesDir(), "groups"));
+        if(files.size() > 0){
+            Log.d("EdtSelectionActivity", "Loading groups from local storage");
+            groupsString = "";
+            for (File file : files) {
+                if (file.getName().endsWith(".html")) {
+                    groupsString += readFile(file) + "\n";
+                }
+            }
+        } else {
+            Log.d("EdtSelectionActivity", "Loading groups from the web");
+            String url = "https://edt.univ-nantes.fr/chantrerie-gavy/gindex.html";
+            UrlRequests urlRequests = new UrlRequests(this, "GROUPS", "gindex.html");
+            urlRequests.execute(url);
+        }
     }
 
     //on resume
@@ -61,6 +77,47 @@ public class EdtSelectionActivity extends AppCompatActivity {
         }
     }
 
+    protected String readFile(File file){
+        Log.d("EdtSelectionActivity", "Loading groups: " + file.getName());
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            // Open FileInputStream for reading
+            FileInputStream inputStream = new FileInputStream(file.getPath());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            // Read file content into StringBuilder
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append('\n');
+            }
+
+            // Close the streams
+            bufferedReader.close();
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuilder.toString();
+    }
+
+    protected static ArrayList<File> getFiles(File directory){
+        ArrayList<File> files = new ArrayList<>();
+        File[] filesArray = directory.listFiles();
+
+        if (filesArray != null) {
+            for (File file : filesArray) {
+                if (file.isDirectory()) {
+                    files.addAll(getFiles(file));
+                } else {
+                    files.add(file);
+                }
+            }
+        }
+
+        return files;
+    }
+
     public void closeActivityAction(View view) {
         finish();
     }
@@ -68,10 +125,10 @@ public class EdtSelectionActivity extends AppCompatActivity {
     private class getGroups extends AsyncTask<Void, Void, Map<String,String> > {
         @Override
         protected Map<String,String>  doInBackground(Void... voids) {
-            Map<String,String> groups = new Hashtable<>();
+            Map<String, String> groups = new Hashtable<>();
 
-            while(groupsString.isEmpty()){
-                try{
+            while (groupsString.isEmpty()) {
+                try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
@@ -81,7 +138,7 @@ public class EdtSelectionActivity extends AppCompatActivity {
             String[] lines = groupsString.split("\n");
 
             for (String line : lines) {
-                if(line.contains("<option value")){
+                if (line.contains("<option value")) {
                     boolean endID = false;
                     boolean endName = false;
                     StringBuilder id = new StringBuilder();
@@ -104,9 +161,13 @@ public class EdtSelectionActivity extends AppCompatActivity {
                         }
                         if (line.charAt(i) == '/') {
                             //Cut id at the dot
-                            id = new StringBuilder(id.substring(0, id.length() - 6));
-                            groups.put(name.toString(), id.toString());
-                            break;
+                            try {
+                                id = new StringBuilder(id.substring(0, id.length() - 6));
+                                groups.put(name.toString(), id.toString());
+                            } catch (Exception e) {
+                                Log.e("EdtSelectionActivity", "Error while parsing groups");
+                                break;
+                            }
                         }
                     }
                 }
